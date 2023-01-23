2ce275cd17c44d2a48b25519a00b57954b4a3e73
regulate <regulate@gmail.com>
2023-01-22 19:19:59 -0800

    [urlshort] Added support for passing yaml file on command line

--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/notemptylist/gophersizes/urlshort"
 )
 
 func main() {
+	yamlFile := flag.String("yaml", "urls.yml", "A yml file with path and url mappings")
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -19,19 +23,16 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-    - path: /urlshort
-      url: https://github.com/gophercises/urlshort
-    - path: /urlshort-final
-      url: https://github.com/gophercises/urlshort/tree/solution
-    `
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yaml, err := os.ReadFile(*yamlFile)
+	if err != nil {
+		panic(err)
+	}
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
-	// http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
