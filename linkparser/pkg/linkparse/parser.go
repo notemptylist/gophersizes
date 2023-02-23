57ca20ad8b80d8efc0f63e25d13fb6812322c2fe
regulate <regulate@gmail.com>
2023-02-22 17:45:21 -0800

    [linkparser] Move text finding into a function

--- linkparser/pkg/linkparse/parser.go
+++ linkparser/pkg/linkparse/parser.go
@@ -28,10 +28,23 @@ func New(r io.Reader) (*LinkParser, error) {
 	return &LinkParser{r: r, node: node}, nil
 }
 
+// findText recursively finds a text element.
+func (l *LinkParser) findText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+
+	var text string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		text += l.findText(c)
+	}
+	return text
+}
+
 // inspectNode inspects a single node to determine if it is a link,
 // it then recursively calls itself on the first child of the node and
 // every sibling of that node.
-func (l *LinkParser) inspectNode(n *html.Node) {
+func (l *LinkParser) inspectNode(n *html.Node, padding string) {
 	// if this node is a link, extract href and text
 	// and create a Link value
 	if n.Type == html.ElementNode && n.Data == "a" {
@@ -44,19 +57,20 @@ func (l *LinkParser) inspectNode(n *html.Node) {
 				break
 			}
 		}
-		if n.FirstChild != nil {
-			link.Text = strings.Trim(n.FirstChild.Data, "\t\n ")
-			l.links = append(l.links, link)
-		}
+		t := l.findText(n)
+		link.Text = strings.Trim(t, "\t\n ")
+		l.links = append(l.links, link)
 	}
 	for child := n.FirstChild; child != nil; child = child.NextSibling {
-		l.inspectNode(child)
+		// descend down the tree first
+		l.inspectNode(child, padding+" ")
+		// when this returns, we will follow siblings and descend their tree
 	}
 }
 
 // EmitLinks returns all the links which were discovered during parsing.
 func (l *LinkParser) EmitLinks() []Link {
 	n := l.node
-	l.inspectNode(n)
+	l.inspectNode(n, "")
 	return l.links
 }
